b6de5062bad39a092c3ed21f48a09addc1885812
Rahul Bharati <rahulrbharati@gmail.com>
2022-08-13 12:24:17 +0530

    Add redux in client app

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
+import { wrapper } from "../store/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,4 +12,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
