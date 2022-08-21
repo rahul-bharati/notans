c872a41f66ecc7e80afbb46f33bd1901a702691e
Rahul Bharati <rahulrbharati@gmail.com>
2022-08-21 13:23:11 +0530

    create notans smartcontract

--- packages/smart_contracts/hardhat.config.ts
+++ packages/smart_contracts/hardhat.config.ts
@@ -1,8 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.ALCHEMY_HTTP,
+      accounts: [process.env.ACCOUNT as string],
+    },
+  },
 };
 
 export default config;
