37878e208c2fb77c8c7590c8669a0a1c11c22546
Rahul Bharati <rahulrbharati@gmail.com>
2022-08-22 11:53:14 +0530

    add functions to connect wallet and check storage token

--- packages/client/context/UtilContext.tsx
+++ packages/client/context/UtilContext.tsx
@@ -1,8 +1,15 @@
 import { useRouter } from "next/router";
-import { createContext, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Service, Web3Storage } from "web3.storage";
 import { ProtectedRoutes } from "../constants";
 import { selectAuthState, setAuthState } from "../store/authStore";
+import { Web3torage_token } from "../utils";
+
+import NotansContractAbi from "../abi/Notans.json";
+import { ethers } from "ethers";
+
+const NOTANS_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_NOTANS_CONTRACT_ADDRESS;
 
 declare global {
   interface Window {
@@ -29,14 +36,57 @@ export const UtilContext = createContext<IUtilContext>({
 });
 
 export const UtilContextProvider = ({ children }: Props) => {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const storageClient = new Web3Storage({ token: Web3torage_token } as Service);
   const dispath = useDispatch();
 
+  const isStorageClientValid = async () => {
+    try {
+      for await (const _ of storageClient.list({ maxResults: 1 })) {
+        // any non-error response means the token is legit
+        break;
+      }
+      return true;
+    } catch (error: any) {
+      // only return false for auth-related errors
+      if (error.message.includes("401") || error.message.includes("403")) {
+        console.log("invalid token", error.message);
+        return false;
+      }
+    }
+  };
   const router = useRouter();
   const authState = useSelector(selectAuthState);
 
   const { asPath } = router;
 
-  const connectWallet = () => {};
+  const connectWallet = async () => {
+    const { ethereum } = window;
+    try {
+      if (!ethereum) return alert("Please install metamask");
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setCurrentAccount(accounts[0]);
+      setWalletConnected(true);
+    } catch (error) {
+      console.log(error);
+      setCurrentAccount("");
+      setWalletConnected(false);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window.ethereum) {
+      window.ethereum.on("disconnect", () => {
+        setWalletConnected(false);
+        setCurrentAccount("");
+      });
+    }
+  }, []);
+
   const storeAuth = (token: string) => {
     if (typeof window !== "undefined") {
       localStorage.setItem("token", token);
@@ -69,6 +119,17 @@ export const UtilContextProvider = ({ children }: Props) => {
       router.push("/login");
     }
   }, [asPath, router, authState]);
+
+  const getNotansContract = () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      NOTANS_CONTRACT_ADDRESS as string,
+      NotansContractAbi.abi,
+      signer
+    );
+    return contract;
+  };
 
   return (
     <UtilContext.Provider
